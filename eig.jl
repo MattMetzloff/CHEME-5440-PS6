4c7f72f85b88e2b5252c0e33c0d80eb51a653232
MattMetzloff <mrm332@cornell.edu>
2021-05-11 22:59:10 -0400

    Update eig.jl

--- eig.jl
+++ eig.jl
@@ -3,8 +3,8 @@ alpha = 10
 n=1
 c_n1=2.702
 
-J = [-1 ((-alpha*n*(c_n1^(n-1)))/(1+(c_n1^(n)))^2)
-((-alpha*n*(c_n1^(n-1)))/(1+(c_n1^(n)))^2) -1]
+J = [-1 (-alpha*n*(c_n1^(n-1)))/((1+(c_n1^(n)))^2)
+(-alpha*n*(c_n1^(n-1)))/((1+(c_n1^(n)))^2) -1]
 
 n1_eigs=eigvals(J)
 display("when n=1,")
@@ -13,8 +13,8 @@ display(n1_eigs)
 n=2
 c_n2=2
 
-J = [-1 ((-alpha*n*(c_n2^(n-1)))/(1+(c_n2^(n)))^2)
-((-alpha*n*(c_n2^(n-1)))/(1+(c_n2^(n)))^2) -1]
+J = [-1 (-alpha*n*(c_n2^(n-1)))/((1+(c_n2^(n)))^2)
+(-alpha*n*(c_n2^(n-1)))/((1+(c_n2^(n)))^2) -1]
 
 n2_eigs=eigvals(J)
 display("when n=2,")
